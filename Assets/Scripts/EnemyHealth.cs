d9a61b9725f11cff5028d2774b60f86f0af460ed
joeface71 <joefacenda@yahoo.com>
2019-08-02 15:36:56 -0400

    level jpg imported

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
+    
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }    
 
     public void TakeDamage(float damage)
     {
@@ -13,7 +20,14 @@ public class EnemyHealth : MonoBehaviour
 
         if (hitPoints <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("Die");        
     }
 }
